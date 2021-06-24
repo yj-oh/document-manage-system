2c19d7ec9b68eaae76f8252bf2abdd55cf343d08
yj-oh <holidayoh99@gmail.com>
2021-06-25 05:17:37 +0900

    [추가] 속성값을 상수로 정의하는 Attributes 클래스

---
+++ src/main/java/com/yjworld/docmng/Attributes.java
@@ -0,0 +1,8 @@
+package main.java.com.yjworld.docmng;
+
+public final class Attributes {
+	public static final String PATH = "path";
+	public static final String WIDTH = "width";
+	public static final String HEIGHT = "height";
+	public static final String TYPE = "type";
+}
