1aad61f7bf6bc00d568e0123945dbe80b2f74385
yj-oh <holidayoh99@gmail.com>
2021-06-25 05:37:44 +0900

    [추가] 텍스트 파일의 내용을 처리하는 TextFile 클래스

---
+++ src/main/java/com/yjworld/docmng/TextFile.java
@@ -0,0 +1,43 @@
+package main.java.com.yjworld.docmng;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+
+import static java.util.stream.Collectors.toList;
+import static main.java.com.yjworld.docmng.Attributes.*;
+
+class TextFile {
+    private final Map<String, String> attributes;
+	private final List<String> lines;
+
+	TextFile(final File file) throws IOException {
+		attributes = new HashMap<>();
+		attributes.put(PATH, file.getPath());
+		lines = Files.lines(file.toPath()).collect(toList());
+	}
+
+	int addLines(
+			final int start,
+			final Predicate<String> isEnd,
+			final String attributeName) {
+		final StringBuilder accumulator = new StringBuilder();
+		int lineNumber;
+
+		for (lineNumber = start; lineNumber < lines.size(); lineNumber++) {
+			final String line = lines.get(lineNumber);
+
+			if (isEnd.test(line)) {
+				break;
+			}
+			accumulator.append(line);
+			accumulator.append("\n");
+		}
+		attributes.put(attributeName, accumulator.toString().trim());
+		return lineNumber;
+	}
+}
