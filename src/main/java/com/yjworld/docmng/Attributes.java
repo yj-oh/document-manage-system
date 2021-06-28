b20a91369906cdc958c3090667860d5f744bf69c
yj-oh <holidayoh99@gmail.com>
2021-06-29 02:46:22 +0900

    [추가] Attributes 클래스에 환자, 주소, 본문 속성값 추가

--- src/main/java/com/yjworld/docmng/Attributes.java
+++ src/main/java/com/yjworld/docmng/Attributes.java
@@ -5,4 +5,7 @@ public final class Attributes {
 	public static final String WIDTH = "width";
 	public static final String HEIGHT = "height";
 	public static final String TYPE = "type";
+	public static final String PATIENT = "patient";
+	public static final String ADDRESS = "address";
+	public static final String BODY = "body";
 }
